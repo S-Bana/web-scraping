2da2da5f20bd54ad5b3bd80f0efff2aa62521413
sajad <sajadbanazadeh@gmai.com>
2024-11-15 13:54:10 +0330

    create a parameter to save html code and by library beautifulsoup select html code and use prettify to show bether codes

--- beautifulsoup/main.py
+++ beautifulsoup/main.py
@@ -1,9 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 # url = 'https://books.toscrape.com/'
 url = 'https://crawler-test.com/'
 
 response = requests.get(url)
 
+# print response code
 print(response.status_code)
+
+# print codes html 
+# print(response.text)
+
+# page_html is a value to have html code website
+page_html = response.text
+
+# prettify can be show code by struct
+soup = BeautifulSoup(markup=page_html, features='html.parser')
+print(soup.prettify())
