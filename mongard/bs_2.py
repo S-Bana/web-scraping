18729ddd6b124e97fe8d970d8e68f2b3f7a8f50a
sajad <sajadbanazadeh@gmail.com>
2024-11-18 13:23:07 +0330

    find something by char

--- mongard/bs_2.py
+++ mongard/bs_2.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
+# =setup====================================================================
 # url = 'https://www.wikipedia.org/'
 url = 'https://en.wikipedia.org/wiki/Linus_Torvalds'
 
@@ -12,6 +14,7 @@ content = BeautifulSoup(response.text, 'html.parser')
 # print source code html
 # print(content)
 
+
 # =find====================================================================
 # find a value and return first one
 # print(content.find('h1'))
@@ -20,19 +23,38 @@ content = BeautifulSoup(response.text, 'html.parser')
 # print(content.find_all('h1'))
 # print(content.findAll('h1'))
 
+# find a value and return all
+# print(content.find_all('h2', limit=5))
+
 # find two or more than values and return first one
 # print(content.find(['h1','h2']))
 
 # find two or more than values and return first one
 # print(content.find_all(['h1','h2']))
 
-# =find_thing==========================================================
+
+# =find_something==========================================================
 
 # find a value and return first one, in result get attribute, text , ...
 # print(content.find('h1').attrs)
 
 # print(content.find('h1').text)
 
-print(content.find(attrs={"title":"Alan Emtage"}))
+# print(content.find(attrs={"title":"Alan Emtage"}))
 
 
+# =find_something_&_filter_it==========================================================
+
+# print(content.find('h1').text)
+
+# print(len(content.find('h2')))
+
+# print(content.find('h2').get('id'))
+
+
+# =find_by_search=====================================================================
+
+# find value , that start by 'd'
+# print(content.find(re.compile('^d')))
+
+# ======================================================================
