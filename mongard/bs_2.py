df8fd28eb7701ca565626c3cbc25e585f08d66b6
sajad <sajadbanazadeh@gmail.com>
2024-11-18 10:00:13 +0330

    find method

---
+++ mongard/bs_2.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://www.wikipedia.org/'
+
+response = requests.get(url=url)
+
+content = BeautifulSoup(response.text, 'html.parser')
+
+# print source code html
+# print(content)
+
+# find a value and return first one
+# print(content.find('h1'))
+
+# find a value and return all
+# print(content.find_all('h1'))
+# print(content.findAll('h1'))
+
+# find two or more than values and return first one
+# print(content.find(['h1','h2']))
+
+# find two or more than values and return first one
+print(content.find_all(['h1','h2']))
